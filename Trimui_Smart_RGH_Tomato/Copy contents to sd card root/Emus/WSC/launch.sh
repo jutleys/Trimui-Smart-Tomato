9dc1842df1e975bbf2fd24a66c9ba56d61358bec
hertman789 <hertman789@gmail.com>
2023-01-20 20:53:48 -0300

    wonderswan fixed

--- Trimui_Smart_RGH_Tomato/Copy contents to sd card root/Emus/WSC/launch.sh
+++ Trimui_Smart_RGH_Tomato/Copy contents to sd card root/Emus/WSC/launch.sh
@@ -2,7 +2,7 @@
 echo "===================================="
 echo $0 $*
 RA_DIR=/mnt/SDCARD/Apps/1-RetroArch
-EMU_DIR=/mnt/SDCARD/Emus/WSC
+EMU_DIR=/mnt/SDCARD/Emus/WS/WSC
 cd $RA_DIR/
 
 HOME=$RA_DIR/ $RA_DIR/ra32.trimui -v $NET_PARAM -L $EMU_DIR/mednafen_wswan_libretro.so "$*"
