1544b3f5b0875b8da0e71ddb450af41acfa9c52d
hertman789 <hertman789@gmail.com>
2023-01-20 19:44:31 -0300

    "Updater" Created

---
+++ Trimui_Smart_RGH_Tomato/Copy contents to sd card root/Apps/UpdateMove/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd $(dirname "$0")
+HOME=/mnt/SDCARD
+
+mv /mnt/SDCARD/.retroarch/system/* /mnt/SDCARD/Bios
+mv /mnt/SDCARD/.retroarch/saves/* /mnt/SDCARD/Saves/saves
+mv /mnt/SDCARD/.retroarch/states/* /mnt/SDCARD/Saves/states
+mv /mnt/SDCARD/.retroarch/screenshots/* /mnt/SDCARD/screenshots
+
+./printstr " Files Moved "
